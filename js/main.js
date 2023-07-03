24f44571fc600f3db906cbd03e7e94a42298a285
Hamidjonova-N <shamsuddinovanigoraxon@gmail.com>
2023-07-03 09:50:58 +0500

    Add country-select

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+let countrySelect = document.querySelector(".country-select");
+let countryInput = document.querySelector(".country-input");
+
+countrySelect.addEventListener("change", function () {
+  if (countrySelect.value === "other") {
+    countryInput.disabled = false;
+  } else {
+    countryInput.value = "";
+    countryInput.disabled = true;
+  }
+});
